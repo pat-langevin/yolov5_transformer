bcaabf0cd097bb774d62567e681d33bb5c1a9374
Patrick Langevin <pat.langevin@hotmail.com>
2021-03-18 23:44:45 -0400

    tracker v2

--- utils/plots.py
+++ utils/plots.py
@@ -21,8 +21,6 @@ from scipy.signal import butter, filtfilt
 from utils.general import xywh2xyxy, xyxy2xywh
 from utils.metrics import fitness
 
-from track import Position
-
 # Settings
 matplotlib.rc('font', **{'size': 3})
 matplotlib.use('Agg')  # for writing to files only
@@ -71,14 +69,14 @@ def plot_one_box(x, img, color=None, label=None, line_thickness=3):
         cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
-def plot_one_box_and_dot(x, img, id, color=None, label=None, line_thickness=3):
+def plot_one_box_and_dot(x, center, img, id, color=None, label=None, line_thickness=3):
     # Plots one bounding box on image img
     # tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
     tl = 1
     # color = color or [random.randint(0, 255) for _ in range(3)]
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
     # image = cv2.circle(image, center_coordinates, radius, color, thickness)
-    cv2.circle(img, c1, 1, color, tl)
+    cv2.circle(img, center, 20, color, tl)
     cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
     if label and id :
         titre = label + "_" + str(id)
